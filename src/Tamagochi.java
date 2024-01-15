76c7edff92b5ddd2ace15e6474a867d2048826eb
KasperFrotzlerElev <128380069+KasperFrotzlerElev@users.noreply.github.com>
2024-01-15 09:35:36 +0100

    första update

---
+++ src/Tamagochi.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Tamagochi {
+    int hunger;
+    int boredom;
+    String words;
+    boolean isAlive;
+    Random generator;
+    public String name;
+
+    public void Feed() {
+
+    }
+    public void Hi(){
+
+    }
+
+    public void Teach() {
+
+    {
+
+    }
+    public void Hi(){
+
+    }
+
+}
