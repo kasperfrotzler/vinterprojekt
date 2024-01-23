3fe15fc75b6208678a4434184b8ea7d22195c46d
KasperFrotzlerElev <128380069+KasperFrotzlerElev@users.noreply.github.com>
2024-01-23 22:13:18 +0100

    andra update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+        Tamagochi CurrentTama;
+        ArrayList<Tamagochi> Tamagochis=new ArrayList<Tamagochi>();
+        System.out.println("Tamagochi");
+        System.out.println("Vad vill du att din Tamagochi ska heta?");
+        String name = scn.nextLine();
+
+        while(
     }
 }
--- src/Tamagochi.java
+++ src/Tamagochi.java
@@ -1,27 +1,76 @@
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Tamagochi {
-    int hunger;
-    int boredom;
-    String words;
-    boolean isAlive;
-    Random generator;
+    /*
+    Attribut
+     */
+    private int hunger;
+    private int boredom;
+    private boolean isAlive = true;
+    private Random random = new Random();
     public String name;
+    private ArrayList<String> words = new ArrayList<String>();
 
+    /*
+    Konstruktor
+     */
+    public Tamagochi(String name) {
+        this.name = name;
+    }
+
+    /*
+    Metoder
+     */
     public void Feed() {
-
-    }
-    public void Hi(){
-
-    }
-
-    public void Teach() {
-
-    {
-
-    }
-    public void Hi(){
+        this.hunger = this.hunger - (random.nextInt(1, 4));
+        if (this.hunger < 0) {
+            this.hunger = 0;
+        }
 
     }
 
+    public void Hi() {
+        ReduceBoredom();
+        if (words.size() > 0) {
+            System.out.println(words.get(random.nextInt(words.size())));
+        } else {
+            System.out.println("Din Tamagochi har inte lärt sig några ord");
+        }
+    }
+
+    public void Teach(String word) {
+        ReduceBoredom();
+        words.add(word);
+    }
+
+    public void Tick() {
+        this.hunger++;
+        this.boredom++;
+        if (hunger > 12 || boredom > 12) {
+            isAlive = false;
+        }
+    }
+
+    public void PrintStatus() {
+        if (this.isAlive == true) {
+            System.out.println("Din Tamagochi lever");
+            System.out.println("Din Tamagochis hunger är: " + this.hunger);
+            System.out.println("Din Tamagochis boredom är: " + this.boredom);
+        } else {
+            System.out.println("Din Tamagochi har dött");
+        }
+    }
+
+    public boolean GetAlive() {
+        return isAlive;
+    }
+
+    private void ReduceBoredom() {
+        this.boredom = this.boredom - (random.nextInt(1, 4));
+        if (this.boredom < 0) {
+            this.boredom = 0;
+        }
+
+    }
 }
