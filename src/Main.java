a718bafd711bdb34eac5af13ca9a6fd0717138e0
KasperFrotzlerElev <128380069+KasperFrotzlerElev@users.noreply.github.com>
2024-01-24 11:02:03 +0100

    fjärde update

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
